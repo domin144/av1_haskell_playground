373b4bdebe1f9a8fc6f07d8ed8a942226afd71db
Dominik Wójt <domin144@o2.pl>
2022-11-27 17:10:39 +0100

    implemented the JSON format

--- av1-transport/src/Json.hs
+++ av1-transport/src/Json.hs
@@ -1,27 +1,53 @@
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Json
   ( decodeBitstream,
     encodeBitstream,
   )
 where
 
-import Common
-  ( ObuBytes,
-    Result,
-    wrapMaybe,
-    wrapResult,
+import Common (ObuBytes, Result, wrapMaybe)
+import Data.Aeson
+  ( FromJSON,
+    Object,
+    ToJSON,
+    Value,
+    decode,
+    encode,
+    object,
+    parseJSON,
+    toJSON,
+    withObject,
+    (.:),
   )
+import Data.Aeson.Types (Parser)
+import Data.ByteString.Lazy (pack, unpack)
 import Data.Word (Word8)
-import ObuHeader
-  ( ObuHeader (..),
-    bitsToBytes,
-    bytesToBits,
-    decodeObuHeader,
-    encodeObuHeader,
-    headerSize,
-  )
+
+newtype Bitstream = Bitstream {getBitstream :: [ObuBytes]}
+
+instance FromJSON Bitstream where
+  parseJSON :: Value -> Parser Bitstream
+  parseJSON = withObject "Bitstream" $ \bitstreamObject ->
+    do
+      obuObjects <- bitstreamObject .: "obus" :: Parser [Object]
+      obus <- mapM parseObuJSON obuObjects
+      return $ Bitstream obus
+    where
+      parseObuJSON :: Object -> Parser ObuBytes
+      parseObuJSON obuObject = obuObject .: "data"
+
+instance ToJSON Bitstream where
+  toJSON :: Bitstream -> Value
+  toJSON (Bitstream obus) = object [("obus", toJSON $ map obuToJSON obus)]
+    where
+      obuToJSON :: ObuBytes -> Value
+      obuToJSON bytes = object [("data", toJSON bytes)]
 
 decodeBitstream :: [Word8] -> Result [ObuBytes]
-decodeBitstream = error "TODO"
+decodeBitstream codedBitstream =
+  wrapMaybe "decode JSON" $ getBitstream <$> decode (pack codedBitstream)
 
 encodeBitstream :: [ObuBytes] -> Result [Word8]
-encodeBitstream = error "TODO"
+encodeBitstream obus = Right $ unpack $ encode $ Bitstream obus
